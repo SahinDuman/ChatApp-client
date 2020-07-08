ac1eb08232cf4ac585644fe11a74d79409293c38
SahinDuman <sahinduman247@gmail.com>
2020-07-08 22:32:17 +0200

    .

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-//export const ENDPOINT:string = process.env.ENDPOINT || 'https://desolate-bayou-47080.herokuapp.com/';
-export const ENDPOINT:string = process.env.ENDPOINT || 'http://localhost:5000/';
+export const ENDPOINT:string = process.env.ENDPOINT || 'https://desolate-bayou-47080.herokuapp.com/';
